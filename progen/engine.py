cb2b134f349d4d8b09bc0f8fa0d1c05bc2092bbd
Sean Francis N. Ballais <sfballais123@gmail.com>
2015-05-03 11:33:56 +0800

    Fixed a few glitches

--- progen/engine.py
+++ progen/engine.py
@@ -7,6 +7,8 @@ from .version import __version__
 import os
 
 class Progen:
+    gen_path = os.path.dirname(os.path.abspath(__file__)) + "/generators/"
+
     def usageHelp(self):
         print("usage: progen [--version] [--help] [--list] [generator]")
         print("\t-v or --version\tDisplays the version number of Progen")
@@ -16,25 +18,22 @@ class Progen:
 
     def listGenerators(self):
         print("List of available generators:")
-        
-        gen_path = os.path.dirname(os.path.abspath(__file__)) + "/generators"
 
-        generators = [gen for gen in listdir(gen_path) if isfile(join(gen_path, gen))]
+        generators = [gen for gen in listdir(self.gen_path) if isfile(join(self.gen_path, gen))]
 
         for gen in generators:
             if gen != "__init__.py":
-                print("- {0}\n".format(gen[:-3]))
+                print("  - {0}".format(gen[:-3]))
 
     def runGenerator(self, generator):
-        try:
-            gen = __import__("generators.%s" % generator, fromlist=["progen.generators"])
-        except ImportError:
-            print("Generator ({0}) not found".format(generator))
-            exit()
+        import imp
+
+        gen = imp.load_source(generator, self.gen_path + generator + ".py")
 
         if hasattr(gen, "folders"): # Folders must be a list
             for folder in gen.folders:
                 os.makedirs(folder)
+                print("Creating {0}...".format(folder))
             print("Finished creating the project files...")
         else:
             gen.main()
